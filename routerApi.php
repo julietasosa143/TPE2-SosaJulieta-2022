64fdf71d70340964a2b0e78ffc8e19c278c336c2
Julieta Sosa <julietasosa143@gmail.com>
2022-11-09 16:09:19 -0300

    router actualizado

--- routerApi.php
+++ routerApi.php
@@ -4,12 +4,9 @@ require_once 'libs/Router.php';
 $router = new Router();
 
 // define la tabla de ruteo
-$router->addRoute('items', 'GET', 'ItemsApiController', 'obtenerItems');
-$router->addRoute('items', 'POST', 'ItemsApiController', 'crearItems');
-$router->addRoute('items/:ID', 'GET', 'ItemsApiController', 'obtenerItem');
-$router->addRoute('categorias', 'GET', 'CategoriesApiController','obtenerCategorias');
-$router->addRoute('categorias','POST','CategoriesApiController','crearCategoria');
-$router->addRoute('categorias/:ID','GET','CategoriesApiController','obetenerItemsByCat');
-
+$router->addRoute('reviews', 'GET', 'ApiController', 'getReviews');
+$router->addRoute('reviews', 'POST', 'ApiController', 'createReview');
+$router->addRoute('reviews/:ID', 'GET', 'ApiController', 'getReview');
+$router->addRoute('reviews/:ID','DELETE','ApiController','deleteReview');
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
