ae10a47808cbd1ad6050ff90d528fb0699b20002
Julieta Sosa <julietasosa143@gmail.com>
2022-11-09 16:54:09 -0300

    templates de smarty

---
+++ templates_c/075b848466766c1619c95f330cc79dee66e8115d_0.file.footer.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.39, created on 2022-11-09 20:53:14
+  from 'C:\Users\julie\OneDrive\Documents\TUDAI\Web 2\xampp\htdocs\web2\TPE2-SosaJulieta-2022\templates\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_636c052a40d4e0_70440657',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '075b848466766c1619c95f330cc79dee66e8115d' => 
+    array (
+      0 => 'C:\\Users\\julie\\OneDrive\\Documents\\TUDAI\\Web 2\\xampp\\htdocs\\web2\\TPE2-SosaJulieta-2022\\templates\\footer.tpl',
+      1 => 1665497163,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_636c052a40d4e0_70440657 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+</body>
+<footer class="border-top">
+    <div class="mx-auto" style="width: 200px;">
+    <p class="text-center lead">¡Gracias por visitar nuestra página, vuelva pronto!</p>
+    </div>
+
+</footer>
+</div>
+
+</html><?php }
+}
---
+++ templates_c/d9a1d7ecd5318378710b33b924707f8f8cbd5ce0_0.file.home.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/* Smarty version 3.1.39, created on 2022-11-09 20:53:14
+  from 'C:\Users\julie\OneDrive\Documents\TUDAI\Web 2\xampp\htdocs\web2\TPE2-SosaJulieta-2022\templates\home.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_636c052a3d8311_04965743',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd9a1d7ecd5318378710b33b924707f8f8cbd5ce0' => 
+    array (
+      0 => 'C:\\Users\\julie\\OneDrive\\Documents\\TUDAI\\Web 2\\xampp\\htdocs\\web2\\TPE2-SosaJulieta-2022\\templates\\home.tpl',
+      1 => 1665873569,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:templates/header.tpl' => 1,
+    'file:templates/footer.tpl' => 1,
+  ),
+),false)) {
+function content_636c052a3d8311_04965743 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:templates/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        
+<h2 class="text-center">¡Bienvenidos!</h2>
+<p class="text-center lead">Aqui podran encontrar los mejores productos para potenciar su encanto a traves del sentido del olfato</p>
+
+<img src="./images/indeximage.jpg" class="rounded mx-auto d-block">
+
+
+<?php $_smarty_tpl->_subTemplateRender("file:templates/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
